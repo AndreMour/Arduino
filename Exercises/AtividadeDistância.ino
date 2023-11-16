f0250089a03d7983f8dfe279b8c93e355b428da8
Andre Luiz Moura <131399179+AndreMour@users.noreply.github.com>
2023-11-16 20:49:28 -0300

    Create Exercises

---
+++ Exercises/AtividadeDistância.ino
@@ -0,0 +1,34 @@
+// C++ code
+//
+
+int TRIGGER =9;
+int ECHO = 8;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(TRIGGER, LOW);
+  int time = pulseIn(ECHO, HIGH);
+  
+  int distance = (0.034*time)/2;
+  
+  Serial.print("A distancia e: ");
+  Serial.println(distance);
+  delay(1000);
+  
+  if(distance > 20){
+    digitalWrite(10, LOW);
+  } else {
+    digitalWrite(10, HIGH);
+  }
+  
+}
